7a6e9b223eaa8c66def1146ef4dfc8e5961072c0
Shree Joshi <shreejoshi1805@gmail.com>
2026-05-06 23:35:47 +0530

    Time: 11 ms (10.82%) | Memory: 18.7 MB (9.86%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        hash_map={}
+
+        for num in nums:
+            if num in hash_map:
+                hash_map[num] +=1
+            else:
+                hash_map[num] = 1
+        for key in hash_map:
+            if hash_map[key] ==1:
+
+                return key
