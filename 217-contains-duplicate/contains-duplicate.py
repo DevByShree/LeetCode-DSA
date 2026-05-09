621e1f4f5eb01092639607392575c79282694c96
Shree Joshi <shreejoshi1805@gmail.com>
2026-05-09 14:34:47 +0530

    Time: 15 ms (76.65%) | Memory: 24.4 MB (92.29%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hash_map = {}
+
+        for num in nums:
+            if num in hash_map:
+                return True
+
+            hash_map[num] = True
+        return False
