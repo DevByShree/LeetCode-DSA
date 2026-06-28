df1127c0b0ae7cacc81ded95769408f3eed80b32
Shree Joshi <shreejoshi1805@gmail.com>
2026-06-28 23:50:52 +0530

    Time: 11 ms (95.96%) | Memory: 12.4 MB (85.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,16 +4,17 @@ class Solution(object):
 
         for key in s:
             if key not in mp:
-                mp[key]=1
+                mp[key] = 1
             else:
-                mp[key]+=1
+                mp[key] +=1
+
         for key in t:
             if key not in mp:
                 return False 
             else:
-                mp[key]-=1
+                mp[key] -=1
         for value in mp.values():
-            if value !=0:
-                return False 
+            if value!=0:
+                return False
         return True 
-        
+
