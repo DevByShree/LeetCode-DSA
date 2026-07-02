2c911af96c34eeb628924a6f5af406a3cbf3726d
Shree Joshi <shreejoshi1805@gmail.com>
2026-07-02 22:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (16.24%) - LeetSync

--- 520-detect-capital/detect-capital.py
+++ 520-detect-capital/detect-capital.py
@@ -1,13 +1,5 @@
 class Solution(object):
     def detectCapitalUse(self, word):
 
-        if word == word.upper():
-            return True
-        elif word == word.lower():
-            return True
-        elif word[0].isupper() and word[1:] == word[1:].lower():
-            return True
-        else:
-            return False
-
+        return (word.isupper() or word.islower() or word.istitle())
         
