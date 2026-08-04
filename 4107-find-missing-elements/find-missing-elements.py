da55c2a3aa61e58a33fd6f3f185bda50165097eb
Shree Joshi <shreejoshi1805@gmail.com>
2026-08-05 01:07:16 +0530

    Time: 10 ms (26.10%) | Memory: 12.3 MB (87.99%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        nums.sort()
+
+        ans = []
+
+        for i in range(len(nums) - 1):
+            for j in range(nums[i] + 1, nums[i + 1]):
+                ans.append(j)
+
+        return ans
