42f0052f9acf9f3a8103b235a734a9a45b3004f8
Shree Joshi <shreejoshi1805@gmail.com>
2026-08-16 00:20:36 +0530

    Time: 12 ms (38.98%) | Memory: 13.6 MB (73.77%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def majorityElement(self, nums):
-        hash_map={}
-        for num in nums:
-            if num in hash_map:
-                hash_map[num] +=1
+        mp ={}
+
+        for i in nums:
+            if i not in mp:
+                mp[i] = 1
             else:
-                hash_map[num] = 1
-        for key,value in hash_map.items():
-            if value > len(nums)//2:
-                return key
+                mp[i] +=1 
+        return max(mp,key=mp.get)
